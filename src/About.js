2da45f77a03bc26a5c39a016a6f272e6fe7e911c
Ruban SriramBabu <s.ruban2000@gmail.com>
2022-06-02 23:09:19 +0530

    UI for Projects (and About)

--- src/About.js
+++ src/About.js
@@ -1,8 +1,15 @@
 import { Typography } from "@mui/material";
+import dp from './dp.jpg'
 
 const About = () => {
     return ( 
-        <Typography variant="h4" marginTop="50px">About me</Typography>
+        <div className="About">
+            <Typography variant="h4" marginTop="50px" marginBottom="20px">About me</Typography>
+            <img src={dp} alt=""/>
+            <Typography align="left" marginLeft="50px" marginTop="20px">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet luctus venenatis lectus magna fringilla urna. Odio tempor orci dapibus ultrices in iaculis nunc sed. Pellentesque id nibh tortor id aliquet lectus. Id ornare arcu odio ut sem nulla pharetra diam. Nec tincidunt praesent semper feugiat. Nulla aliquet porttitor lacus luctus accumsan. Facilisis magna etiam tempor orci. Vulputate dignissim suspendisse in est ante in nibh. Cras sed felis eget velit aliquet sagittis.
+            </Typography>
+        </div>
      );
 }
  
--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,84 @@
 import { Typography } from "@mui/material";
+import { Paper } from "@mui/material";
+import bandcamp from './bandcamp.jpg'
+import blockchain from './blockchain.jpg'
+import ghotoverflow from './ghotoverflow.jpg'
+import swiggo from './swiggo.jpg'
+import youtubedl from './youtube-dl.jpg'
+import { Grid } from "@mui/material";
+import { Link } from 'react-router-dom';
+import { styled } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fafafa',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    color: theme.palette.text.secondary,
+  }));
 
 const Projects = () => {
+
+    const projects = [
+        {
+            title: "Bandcamp",
+            description: "The quick brown fox jumps over the lazy dog",
+            imgsrc: bandcamp,
+            link: "https://www.google.co.in"
+        },
+        {
+            title: "Blockchain",
+            description: "The quick brown fox jumps over the lazy dog",
+            imgsrc: blockchain,
+            link: "https://www.google.co.in"
+        },
+        {
+            title: "Ghotoverflow",
+            description: "The quick brown fox jumps over the lazy dog",
+            imgsrc: ghotoverflow,
+            link: "https://www.google.co.in"
+        },
+        {
+            title: "Swiggo",
+            description: "The quick brown fox jumps over the lazy dog",
+            imgsrc: swiggo,
+            link: "https://www.google.co.in"
+        },
+        {
+            title: "youtube-dl",
+            description: "The quick brown fox jumps over the lazy dog",
+            imgsrc: youtubedl,
+            link: "https://www.google.co.in"
+        },
+        {
+            title: "this site lol",
+            description: "The quick brown fox jumps over the lazy dog",
+            imgsrc: "./lite.jpg",
+            link: "https://www.google.co.in"
+        },
+    ]
+
     return ( 
-        <Typography variant="h4" marginTop="50px">Projects I've done</Typography>
+        <div className="Projects">
+            <Typography variant="h4" marginTop="50px" marginBottom="20px">Projects I've done</Typography>
+            <Grid container direction="row" align="left">
+                {
+                    projects.map((project) => (
+                        <Grid item xs={12} borderRadius="10px" marginLeft="50px" marginRight="50px" marginBottom="50px" marginTop="50">
+                            <Item>
+                                <Typography variant="h4" align="center">{project.title}</Typography>
+                                <Grid align="center" marginTop="10px" marginBottom="10px">
+                                    <img src={project.imgsrc} alt=""/>
+                                </Grid>
+                                <Typography marginLeft="10px">{project.description}</Typography>
+                                <Typography marginLeft="10px">
+                                    <Link to={project.link} style={{ color: '#646464' }}>Github Repository</Link>
+                                </Typography>
+                            </Item>
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        </div>
      );
 }
  
